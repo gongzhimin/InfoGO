0be1c6ddd9fcb3f1c77aa886d89b8d47400ad357
The-MinGo <3047673038@qq.com>
2021-10-21 09:51:52 +0800

    add some class

---
+++ infogo/components/email_body.py
@@ -0,0 +1,3 @@
+
+class EmailBody():
+    pass
--- infogo/infogo.py
+++ infogo/infogo.py
@@ -1,4 +1,30 @@
 class InfoGo():
     
     def __init__(self):
+        self.sender()
+
+
+    def get_receiver_list(self):
         pass
+
+    def get_addressor_info(self):
+        pass
+
+
+
+
+
+class Receiver():
+    def __init__(self, full_name, email, sur_name=None, given_name=None):
+        self.full_name = None
+        self.email = None
+        self.degree = None
+        self.sur_name = None
+        self.given_name = None
+    
+
+class Addressor():
+    def __init__(self):
+        self.full_name = None
+        self.email = None
+        self.author_code = None
